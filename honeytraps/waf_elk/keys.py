f79c313cfb85fad542b09506be5505fb023f9f5c
Soyoung Yoo <yooso020599@naver.com>
2020-01-12 04:16:43 +0000

    01/12

--- honeytraps/waf_elk/keys.py
+++ honeytraps/waf_elk/keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-misp_url ='https://3.90.107.89'
+misp_url ='https://52.55.249.172'
 misp_key = 'SZXuQCqUX8WI7ySfa3vfIwiqUN4Xxx9ZVhmnIcCj' # The MISP auth key can be found on the MISP web interface under the automation section
 misp_verifycert = False
--- misp-doc/keys.py
+++ misp-doc/keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-misp_url = 'https://localhost'
-misp_key = 'pfsoob9mktcqSsvb6McHW12Lu8RUA4qvvhhX1Z7t' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_url = 'https://52.55.249.172'
+misp_key = 'SZXuQCqUX8WI7ySfa3vfIwiqUN4Xxx9ZVhmnIcCj' # The MISP auth key can be found on the MISP web interface under the automation section
 misp_verifycert = False
