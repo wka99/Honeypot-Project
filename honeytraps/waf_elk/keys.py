c1c28349f97f0a02452fe81acdc09b0518c5727e
wka99 <43545606+wka99@users.noreply.github.com>
2020-02-28 20:20:48 +0900

    Update keys.py

--- honeytraps/waf_elk/keys.py
+++ honeytraps/waf_elk/keys.py
@@ -2,5 +2,5 @@
 # -*- coding: utf-8 -*-
 
 misp_url ='https://52.55.249.172'
-misp_key = 'SZXuQCqUX8WI7ySfa3vfIwiqUN4Xxx9ZVhmnIcCj' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_key = '81c8LnPvWgVpnjhQorHT04sN96kOnNKBUKvsFfjx' # The MISP auth key can be found on the MISP web interface under the automation section
 misp_verifycert = False
